80483f4a4d046b094765502ca3b0669f063de62f
DELMAS <delmastristan64@gmail.com>
2023-11-08 15:02:41 +0100

    feat: add openable nav burger

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,60 @@
-import React from "react";
+import { useState } from "react";
 import logoImage from "../assets/icons/logo.svg";
 import filterImage from "../assets/icons/filter.svg";
 import searchImage from "../assets/icons/search.svg";
 import navBurger from "../assets/icons/burger.svg";
+import quit from "../assets/icons/quit.svg";
 import "./Navbar.scss";
+import OverlayBurger from "./OverlayBurger";
 
 function Navbar() {
+  const [navBurgerOpen, setNavBurgerOpen] = useState(false);
+
   return (
-    <nav className="navbar">
-      <div className="burger-logo">
-        <button type="button" className="nav-burger">
-          <img src={navBurger} alt="navigation logo" />
-        </button>
-        <div className="logo">
-          <img src={logoImage} alt="cocktail-atom logo" />
-          <p>Cocktail-atom</p>
+    <>
+      <nav className="navbar">
+        <div className="burger-logo">
+          <button
+            type="button"
+            onClick={() => setNavBurgerOpen(!navBurgerOpen)}
+            className="nav-burger"
+          >
+            {navBurgerOpen ? (
+              <img src={quit} alt="navigation logo" />
+            ) : (
+              <img src={navBurger} alt="navigation logo" />
+            )}
+          </button>
+          <div className="logo">
+            <img src={logoImage} alt="cocktail-atom logo" />
+            <p>Cocktail-atom</p>
+          </div>
         </div>
-      </div>
-      <div className="filter-search">
-        <button type="button" className="filter">
-          <img src={filterImage} alt="filter icon" />
-          <p>Filter</p>
-        </button>
-        <div className="search">
-          <input placeholder="search" type="text" />
-          <img className="search-mobile" src={searchImage} alt="search icon" />
-          <img className="search-desktop" src={searchImage} alt="search icon" />
+        <div className="filter-search">
+          <button type="button" className="filter">
+            <img src={filterImage} alt="filter icon" />
+            <p>Filter</p>
+          </button>
+          <div className="search">
+            <input placeholder="search" type="text" />
+            <img
+              className="search-mobile"
+              src={searchImage}
+              alt="search icon"
+            />
+            <img
+              className="search-desktop"
+              src={searchImage}
+              alt="search icon"
+            />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <OverlayBurger
+        isOpen={navBurgerOpen}
+        onClose={() => setNavBurgerOpen(!navBurgerOpen)}
+      />
+    </>
   );
 }
 
